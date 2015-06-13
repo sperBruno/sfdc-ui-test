d0ba5b9f029a52e9869ca71a27db55c6422999e2
6chelo6 <6chelo6@gmail.com>
2015-06-13 18:50:41 -0400

    Adding Campaigns, improving NavigationBar

---
+++ src/main/java/com/salesforce/dev/pages/Campaigns/CampaignsHome.java
@@ -0,0 +1,62 @@
+package com.salesforce.dev.pages.Campaigns;
+
+import com.salesforce.dev.framework.DriverManager;
+import com.salesforce.dev.pages.Base.TabPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Marcelo.Vargas on 13-06-15.
+ */
+public class CamapaignsHome extends TabPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public CamapaignsHome(WebDriver driver) {
+        this.driver = driver;
+        this.wait = DriverManager.getInstance().getWait();
+        PageFactory.initElements(driver, this);
+    }
+
+    public Object clickNewBtn(){
+        super.wait.until(ExpectedConditions.visibilityOf(newBtn));
+        newBtn.click();
+
+        return new CampaignForm(this.driver);
+    }
+    /*
+    public CampaignNewView clickCreateNewCampaignView() {
+        createNewCampaignViewLink.click();
+        return new CampaignNewView(driver);
+    }
+    public boolean verifyTextInList(String text) {
+        dropdown = new Select(viewList);
+        for (WebElement element : dropdown.getOptions()) {
+            if (element.getText().equals(text)) {
+                System.out.println("Element: " + element.getText() + " **EQUAL** " + text);
+                return true;
+            }
+            System.out.println("Element: " + element.getText() + " != " + text);
+        }
+        return false;
+    }
+
+    public void SelectCreatedView(String text) {
+        dropdown = new Select(viewList);
+        dropdown.selectByVisibleText(text);;
+    }
+    public void deleteView() {
+        deleteViewLink.click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }*/
+
+    public Object clickNewBtn() {
+        super.wait.until(ExpectedConditions.visibilityOf(newBtn));
+        newBtn.click();
+        return new CampaignForm(this.driver);
+    }
+}
