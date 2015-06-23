7df3a0978348f2760fe29464b7c68696fe1ecd79
wMercadoJ <soi.Luffy@gmail.com>
2015-06-22 23:15:10 -0400

    Adding Logout Test case Modifying gradle.properties to handle the Display Name of the user Modifying Home and Login Page to use logout method

---
+++ src/test/java/com/salesforce/dev/LogoutTest.java
@@ -0,0 +1,52 @@
+package com.salesforce.dev;
+
+/**
+ * Created by Luffy on 22/06/2015.
+ */
+
+import com.salesforce.dev.framework.Environment;
+import com.salesforce.dev.pages.Home.HomePage;
+import com.salesforce.dev.pages.Home.LoginPage;
+
+import com.salesforce.dev.pages.MainPage;
+import com.salesforce.dev.pages.TopHeader;
+import org.junit.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by Walter on 13/06/2015.
+ */
+public class LogoutTest {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+    private MainPage mainPage;
+
+
+    @BeforeMethod(groups = {"Regression"})
+    public void setUp() {
+        homePage = new HomePage();
+        String userNameValue= Environment.getInstance().getPrimaryUser();
+        String passwordValue=Environment.getInstance().getPrimaryPassword();
+        mainPage = homePage.loginAs(userNameValue,passwordValue);
+    }
+
+    @Test(groups = {"BVT"})
+    public void testCreateAccount() {
+        TopHeader topHeader = mainPage.gotoTopHeader();
+        String displayName= Environment.getInstance().getDisplayName();
+
+        Assert.assertEquals(topHeader.isUserMenuPresent(),true);
+        Assert.assertEquals(topHeader.getUserName(),displayName);
+        topHeader.clickUserNameMenu();
+        LoginPage loginPage = topHeader.clickLogoutOption();
+
+        Assert.assertEquals(loginPage.isLoginButtonPresent(),true);
+
+    }
+
+}
+
