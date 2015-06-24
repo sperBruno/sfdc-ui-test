d324e50d2c2bb889dd982ab3c0034e7f5c77a8a0
wMercadoJ <soi.Luffy@gmail.com>
2015-06-24 14:12:01 -0400

    Update of DataDriven Test and class to use Iterator<>
    - New implementation of DD usind Iterators(Iterator<Account[]>)
    - Adding Accounts class to map json file in a Object
    - Locate accounts.json in Resources folder
    - Locate DataDrivenManager class in framewokr folder

---
+++ src/main/java/com/salesforce/dev/framework/DataDrivenManager.java
@@ -0,0 +1,51 @@
+package com.salesforce.dev.framework;
+
+/**
+ * Created by Walter Mercado on 6/22/2015.
+ */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.salesforce.dev.framework.Objects.Account;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class DataDrivenManager {
+    private JSONParser parser;
+
+    public DataDrivenManager(){
+    }
+
+    public Iterator<Account[]> getAccountsDD(){
+
+        Collection<Account[]> accountsArray =new ArrayList<Account[]>();
+        try {
+            parser = new JSONParser();
+            Object jsonObject = parser.parse(new FileReader("src\\test\\resources\\accounts.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject;
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            List<Account> navigation = objectMapper.readValue(jsonArray.toJSONString(),
+                    objectMapper.getTypeFactory().constructCollectionType(
+                            List.class, Account.class));
+
+            for (int i = 0 ; i< navigation.size(); i++){
+                Account account = (Account)navigation.get(i);
+                accountsArray.add(new Account[]{account});
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return accountsArray.iterator();
+    }
+
+}
---
+++ src/main/java/com/salesforce/dev/framework/Objects/Account.java
@@ -0,0 +1,30 @@
+package com.salesforce.dev.framework.Objects;
+
+/**
+ * Created by Walter Mercado on 6/24/2015.
+ */
+public class Account {
+    private String accountName;
+    private String accountDesc;
+
+    public Account(){}
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getAccountDesc() {
+        return accountDesc;
+    }
+
+    public void setAccountDesc(String accountDesc) {
+        this.accountDesc = accountDesc;
+    }
+
+
+
+}
