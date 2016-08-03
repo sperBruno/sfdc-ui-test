cb3b4f66b79b348190e88114f5371f0f798cec31
Carlos Gonzales <carledriss@gmail.com>
2016-08-03 14:20:29 -0400

    Fixing broken classes Updating build.gradle

--- src/main/java/com/salesforce/dev/pages/Accounts/AccountViewDetail.java
+++ src/main/java/com/salesforce/dev/pages/Accounts/AccountViewDetail.java
@@ -1,25 +1,18 @@
 package com.salesforce.dev.pages.accounts;
 
 import com.salesforce.dev.pages.base.ViewDetailBase;
-import org.openqa.selenium.WebDriver;
-
-import static com.salesforce.dev.framework.selenium.CommonOperation.waitForWebElement;
 
 /**
  * This class will be used to represent Account view details and its options.
  *
  * @author Carlos Orellana.
- * @since 9/2/2015.
  */
 public class AccountViewDetail extends ViewDetailBase {
-    public AccountViewDetail(WebDriver driver) {
-        waitForWebElement(viewSelected);
-    }
 
     @Override
     protected AccountView clickEditLnk() {
         clickEditLink();
-        return new AccountView(driver);
+        return new AccountView();
     }
 
     @Override
