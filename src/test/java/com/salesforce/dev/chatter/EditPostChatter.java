3c21c8ce3ea364ca0928fc274fa5831d82ce5509
DanielRGonzalesV <drgvandroid@gmail.com>
2016-08-03 17:57:38 -0400

    Fix observations

--- src/test/java/com/salesforce/dev/chatter/EditPostChatter.java
+++ src/test/java/com/salesforce/dev/chatter/EditPostChatter.java
@@ -2,6 +2,7 @@ package com.salesforce.dev.chatter;
 
 import com.salesforce.dev.framework.dto.Chatter;
 import com.salesforce.dev.framework.utils.DataDrivenManager;
+import com.salesforce.dev.framework.utils.JSONMapper;
 import com.salesforce.dev.pages.LoginPage;
 import com.salesforce.dev.pages.MainPage;
 import com.salesforce.dev.pages.base.NavigationBar;
@@ -22,6 +23,9 @@ import java.util.Iterator;
  * @since 9/3/2015
  */
 public class EditPostChatter {
+
+    private static final JSONMapper JSON_MAPPER_INSTANCE = JSONMapper.getInstance();
+
     private ChatterHome chatterHome;
     private Chatter createChatter;
 
@@ -46,8 +50,8 @@ public class EditPostChatter {
     }
 
     private Chatter getChatter(String fileJson) {
-        Iterator<Object[]> chattersData = DataDrivenManager.getObjects(fileJson, Chatter.class);
-        return (Chatter) chattersData.next()[0];
+        Chatter chatter = (Chatter) JSON_MAPPER_INSTANCE.getGeneric(new Chatter(),fileJson);
+        return  chatter;
     }
 
     @AfterMethod(groups = {"Acceptance"})
